c0d72283aa61c3408b502cb624c113e1062e9a7f
Santi Mera <santigomero@gmail.com>
2024-08-13 21:26:58 +0300

    Fixed minor bugs and added the responsiveness

--- src/components/PdfViewer.jsx
+++ src/components/PdfViewer.jsx
@@ -2,7 +2,7 @@ import pdfFile from "../assets/Pizza.pdf";
 import classes from "../styles/pdf.module.css"
 
 const PdfViewer = () => {
-    return (<div className={classes.frame} style={{ width: '100%', height: '800px' }}>
+    return (<div className={classes.frame}>
         <iframe
             src={pdfFile}
             title="PDF Preview"
